17d5a352bf92699bb2c5c4a4516bba9c2ff55aad
zhao-feifei <398983914@qq.com>
2021-08-25 15:48:03 +0800

    封装树操作栏组件

---
+++ src/views/departments/components/tree-tolls.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-row
+    type="flex"
+    justify="space-between"
+    align="middle"
+    style="height: 40px;width: 100%"
+  >
+    <el-col>
+      <!-- 名称应该变成 对应的节点中的name -->
+      <span>{{ treeNode.name }}</span>
+    </el-col>
+    <el-col :span="4">
+      <el-row type="flex" justify="end">
+        <!-- 两个内容 -->
+        <el-col>{{ treeNode.manager }}</el-col>
+        <el-col>
+          <!-- 下拉菜单 element -->
+          <el-dropdown>
+            <span> 操作<i class="el-icon-arrow-down" /> </span>
+            <!-- 下拉菜单 -->
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>添加子部门</el-dropdown-item>
+              <el-dropdown-item v-if="!isRoot">编辑部门</el-dropdown-item>
+              <el-dropdown-item v-if="!isRoot">删除部门</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-col>
+      </el-row>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+// 该组件需要对外开放属性 外部需要提供一个对象 对象里需要有name  manager
+export default {
+  // props可以用数组来接收数据 也可以用对象来接收
+  // props: {   props属性: {  配置选项 }  }
+  props: {
+    //   定义一个props属性
+    treeNode: {
+      type: Object, // 对象类型
+      required: true // 要求对方使用您的组件的时候 必须传treeNode属性 如果不传 就会报错
+    },
+    isRoot: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -4,32 +4,7 @@
       <!-- 组织架构内容 -->
       <el-card class="tree-card">
         <!-- 用了一个行列布局 -->
-        <el-row
-          type="flex"
-          justify="space-between"
-          align="middle"
-          style="height: 40px"
-        >
-          <el-col>
-            <span>江苏传智播客教育科技股份有限公司</span>
-          </el-col>
-          <el-col :span="4">
-            <el-row type="flex" justify="end">
-              <!-- 两个内容 -->
-              <el-col>负责人</el-col>
-              <el-col>
-                <!-- 下拉菜单 element -->
-                <el-dropdown>
-                  <span> 操作<i class="el-icon-arrow-down" /> </span>
-                  <!-- 下拉菜单 -->
-                  <el-dropdown-menu slot="dropdown">
-                    <el-dropdown-item>添加子部门</el-dropdown-item>
-                  </el-dropdown-menu>
-                </el-dropdown>
-              </el-col>
-            </el-row>
-          </el-col>
-        </el-row>
+        <TreeTools :tree-node="company" :isRoot="true"></TreeTools>
         <el-tree
           :data="departs"
           :props="defaultProps"
@@ -37,42 +12,7 @@
         >
           <!-- 传入内容 插槽内容 会循环多次 有多少节点 就循环多少次 -->
           <!-- 作用域插槽 slot-scope="obj" 接收传递给插槽的数据   data 每个节点的数据对象-->
-          <el-row
-            slot-scope="{ data }"
-            type="flex"
-            justify="space-between"
-            align="middle"
-            style="height: 40px; width: 100%"
-          >
-            <el-col>
-              <!-- 左侧内容 -->
-              <span>{{ data.name }}</span>
-            </el-col>
-            <el-col :span="4">
-              <el-row type="flex" justify="end">
-                <el-col>{{ data.manager }}</el-col>
-                <el-col>
-                  <!-- 放置下拉菜单 -->
-                  <el-dropdown>
-                    <!-- 内容 -->
-                    <span
-                      >操作
-                      <i class="el-icon-arrow-down" />
-                    </span>
-                    <!-- 具名插槽 -->
-                    <el-dropdown-menu slot="dropdown">
-                      <!-- 下拉选项 -->
-                      <el-dropdown-item>添加子部门</el-dropdown-item>
-                      <el-dropdown-item>编辑部门</el-dropdown-item>
-                      <el-dropdown-item>删除部门</el-dropdown-item>
-                    </el-dropdown-menu>
-                  </el-dropdown>
-                </el-col>
-              </el-row>
-
-              <!-- 右侧内容 -->
-            </el-col>
-          </el-row>
+          <TreeTools slot-scope="{ data }" :tree-node="data"></TreeTools>
         </el-tree>
       </el-card>
     </div>
@@ -80,9 +20,14 @@
 </template>
 
 <script>
+import TreeTools from './components/tree-tolls'
 export default {
+  components: {
+    TreeTools
+  },
   data() {
     return {
+      company: { name: '江苏传智播客教育科技股份有限公司', manager: '负责人' }, //头部的数据结构
       departs: [
         {
           name: '总裁办',
