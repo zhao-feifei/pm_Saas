ac2a29d4819295fb52ddc58fdc2da8369394777c
zhao-feifei <398983914@qq.com>
2021-08-26 09:30:16 +0800

    完成表单校验前置条件

--- src/views/departments/components/add-dept.vue
+++ src/views/departments/components/add-dept.vue
@@ -1,22 +1,34 @@
 <template>
   <!-- 弹层组件 -->
   <el-dialog title="新增部门" :visible="showDialog">
-    <el-form :model="form" label-width="120px">
-      <el-form-item label="部门名称">
-        <el-input style="width:80%" placeholder=""></el-input>
+    <el-form :model="formData" :rules="rules" label-width="120px">
+      <el-form-item prop="name" label="部门名称">
+        <el-input
+          v-model="formData.name"
+          style="width:80%"
+          placeholder=""
+        ></el-input>
       </el-form-item>
-      <el-form-item label="部门编码">
-        <el-input style="width:80%" placeholder=""></el-input>
+      <el-form-item prop="code" label="部门编码">
+        <el-input
+          v-model="formData.code"
+          style="width:80%"
+          placeholder=""
+        ></el-input>
       </el-form-item>
-      <el-form-item label="部门负责人">
-        <el-select style="width:80%" placeholder="请选择" />
+      <el-form-item prop="manager" label="部门负责人">
+        <el-select
+          v-model="formData.manager"
+          style="width:80%"
+          placeholder="请选择"
+        />
       </el-form-item>
-
-      <el-form-item label="部门介绍">
+      <el-form-item prop="introduce" label="部门介绍">
         <el-input
           style="width:80%"
           placeholder="1-300个字符"
           type="textarea"
+          v-model="formData.introduce"
           :rows="3"
         ></el-input>
       </el-form-item>
@@ -38,6 +50,17 @@ export default {
     showDialog: {
       type: Boolean,
       default: false
+    }
+  },
+  data() {
+    return {
+      formData: {
+        name: '',
+        code: '',
+        manager: '',
+        introduce: ''
+      },
+      rules: {}
     }
   }
 }
