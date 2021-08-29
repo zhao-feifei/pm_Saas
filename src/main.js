1dbac338ffada1f28cbb7956a0281054fb509153
zhao-feifei <398983914@qq.com>
2021-08-29 15:34:04 +0800

    员工布局完成

--- src/main.js
+++ src/main.js
@@ -11,20 +11,10 @@ import '@/styles/index.scss' // global css
 import App from './App'
 import store from './store'
 import router from './router'
-
+import Component from '@/components'
 import '@/icons' // icon
 import '@/permission' // permission control
-
-/**
- * If you don't want to use mock-server
- * you want to use MockJs for mock api
- * you can execute: mockXHR()
- *
- * Currently MockJs will be used in the production environment,
- * please remove it before going online ! ! !
- */
-
-// set ElementUI lang to EN
+Vue.use(Component) // 注册自己的插件
 Vue.use(ElementUI, { locale })
 // 如果想要中文版 element-ui，按如下方式声明
 // Vue.use(ElementUI)
--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -1,9 +1,49 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        员工
-      </h2>
+      <page-tools :show-before="true">
+        <template v-slot:before>
+          <span slot="before">共16条数据记录</span>
+        </template>
+        <template v-slot:after>
+          <el-button size="small" type="success">导入</el-button>
+          <el-button size="small" type="danger">导出</el-button>
+          <el-button size="small" type="primary">新增员工</el-button>
+        </template>
+      </page-tools>
+      <!-- 放置表格和分页 -->
+      <el-card>
+        <el-table border>
+          <el-table-column label="序号" sortable="" />
+          <el-table-column label="姓名" sortable="" />
+          <el-table-column label="工号" sortable="" />
+          <el-table-column label="聘用形式" sortable="" />
+          <el-table-column label="部门" sortable="" />
+          <el-table-column label="入职时间" sortable="" />
+          <el-table-column label="账户状态" sortable="" />
+          <el-table-column label="操作" sortable="" fixed="right" width="280">
+            <template>
+              <el-button type="text" size="small">查看</el-button>
+              <el-button type="text" size="small">转正</el-button>
+              <el-button type="text" size="small">调岗</el-button>
+              <el-button type="text" size="small">离职</el-button>
+              <el-button type="text" size="small">角色</el-button>
+              <el-button type="text" size="small">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+
+        <!-- 分页 -->
+        <el-row
+          type="flex"
+          justify="center"
+          align="middle"
+          style="height: 60px"
+        >
+          <el-pagination layout="prev, pager, next" :total="50">
+          </el-pagination>
+        </el-row>
+      </el-card>
     </div>
   </div>
 </template>
