ecd991fa8d24ce50e2ba43560a21fe1558b7b864
zhao-feifei <398983914@qq.com>
2021-09-07 14:54:52 +0800

    封装个人详情组件

--- src/views/employees/detail.vue
+++ src/views/employees/detail.vue
@@ -26,7 +26,11 @@
               </el-form-item>
             </el-form>
           </el-tab-pane>
-          <el-tab-pane label="个人详情"></el-tab-pane>
+          <el-tab-pane label="个人详情">
+            <!-- 放置个人详情 -->
+            <component :is="userComponent" />
+            <!-- <user-info /> -->
+          </el-tab-pane>
           <el-tab-pane label="岗位信息"></el-tab-pane>
         </el-tabs>
       </el-card>
@@ -37,9 +41,14 @@
 <script>
 import { getUserDetailById } from '@/api/user'
 import { saveUserDetailById } from '@/api/employees'
+import UserInfo from './components/user-info'
 export default {
+  components: {
+    UserInfo
+  },
   data() {
     return {
+      userComponent: 'UserInfo',
       userId: this.$route.params.id, // 这样可以后面直接通过 this.userId进行获取数据
       userInfo: {
         //   专门存放基本信息
