17c3ec8c634d1039cbea66e68617ba673a9666e3
zhao-feifei <398983914@qq.com>
2021-09-07 08:34:01 +0800

    处理日期格式完成

--- src/views/import/index.vue
+++ src/views/import/index.vue
@@ -12,26 +12,55 @@ export default {
   },
   methods: {
     async success({ header, results }) {
-      // 如果是导入员工
-
-      const userRelations = {
-        入职日期: 'timeOfEntry',
-        手机号: 'mobile',
-        姓名: 'username',
-        转正日期: 'correctionTime',
-        工号: 'workNumber'
-      }
-      console.log(results)
-      const arr = []
-      results.forEach(item => {
-        const userInfo = {}
-        Object.keys(item).forEach(key => {
-          userInfo[userRelations[key]] = item[key]
+      if (this.type === 'user') {
+        const userRelations = {
+          入职日期: 'timeOfEntry',
+          手机号: 'mobile',
+          姓名: 'username',
+          转正日期: 'correctionTime',
+          工号: 'workNumber'
+        }
+        const arr = []
+        // 遍历所有的数组
+        results.forEach(item => {
+          // 需要将每一个条数据里面的中文都换成英文
+          const userInfo = {}
+          Object.keys(item).forEach(key => {
+            // key是当前的中文名 找到对应的英文名
+            if (
+              userRelations[key] === 'timeOfEntry' ||
+              userRelations[key] === 'correctionTime'
+            ) {
+              userInfo[userRelations[key]] = new Date(
+                this.formatDate(item[key], '/')
+              ) // 只有这样, 才能入库
+              return
+            }
+            userInfo[userRelations[key]] = item[key]
+          })
+          // 最终userInfo变成了全是英文
+          arr.push(userInfo)
         })
-        arr.push(userInfo)
-      })
-      await importEmployee(arr) // 调用导入接口
-      this.$router.back()
+        await importEmployee(arr)
+        this.$message.success('导入成功')
+      }
+      this.$router.back() // 回到上一页
+    },
+    //转换excel的日期格式
+    formatDate(numb, format) {
+      const time = new Date((numb - 1) * 24 * 3600000 + 1)
+      time.setYear(time.getFullYear() - 70)
+      const year = time.getFullYear() + ''
+      const month = time.getMonth() + 1 + ''
+      const date = time.getDate() - 1 + ''
+      if (format && format.length === 1) {
+        return year + format + month + format + date
+      }
+      return (
+        year +
+        (month < 10 ? '0' + month : month) +
+        (date < 10 ? '0' + date : date)
+      )
     }
   }
 }
