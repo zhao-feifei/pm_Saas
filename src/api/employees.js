f9ebeb077c286010e35a692cb30b2a36de96b13d
zhao-feifei <398983914@qq.com>
2021-09-07 11:15:24 +0800

    封装用户信息接口

--- src/api/employees.js
+++ src/api/employees.js
@@ -39,3 +39,12 @@ export function importEmployee(data) {
     data
   })
 }
+
+//保存用户基本信息
+export function saveUserDetailById(data) {
+  return request({
+    url: `/sys/user/${data.id}`,
+    method: 'put',
+    data
+  })
+}
