b3ecee94e51dd39d0c3500d8b11b0d3ca3e26a5d
zhao-feifei <398983914@qq.com>
2021-09-07 18:53:23 +0800

    上传前校验完成

--- src/components/ImageUpload/index.vue
+++ src/components/ImageUpload/index.vue
@@ -5,7 +5,10 @@
       :file-list="fileList"
       list-type="picture-card"
       action="#"
+      :on-remove="handleRemove"
+      :on-change="changeFile"
       :limit="1"
+      :before-upload="beforeUpload"
       :class="{ disabled: fileComputed }"
     >
       <i class="el-icon-plus"></i>
@@ -42,6 +45,31 @@ export default {
       // 这里应该弹出一个层 层里是点击的图片地址
       this.imgUrl = file.url
       this.showDialog = true
+    },
+    handleRemove(file, fileList) {
+      this.fileList = this.fileList.filter(item => item.uid !== file.uid)
+    },
+    // 修改文件时触发
+    // 此时可以用fileList 因为该方法会进来很多遍 不能每次都去push
+    // fileList因为fileList参数是当前传进来的最新参数 我们只需要将其转化成数组即可 需要转化成一个新的数组
+    // [] => [...fileList] [] => fileList.map()
+    // 上传成功之后 还会进来 需要实现上传代码的逻辑 这里才会成功
+    changeFile(file, fileList) {
+      this.fileList = fileList.map(item => item)
+    },
+    beforeUpload(file) {
+      const types = ['image/jpeg', 'image/gif', 'image/bmp', 'image/png']
+      if (!types.includes(file.type)) {
+        this.$message.error('上传图片只能是 JPG、GIF、BMP、PNG 格式!')
+        return false
+      }
+      //限制上传的图片大小
+      const maxSize = 5 * 1024 * 1024
+      if (maxSize < file.size) {
+        this.$message.error('图片大小最大不能超过5M')
+        return false
+      }
+      return true
     }
   }
 }
