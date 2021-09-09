6b46ebf354bd8f61691ade8ec97ba45d2d581705
zhao-feifei <398983914@qq.com>
2021-09-09 09:08:44 +0800

    封装权限接口

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -1,9 +1,24 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        权限管理
-      </h2>
+      <page-tools>
+        <template v-slot:after>
+          <el-button type="primary">添加权限</el-button>
+        </template>
+      </page-tools>
+      <!-- 表格 -->
+      <el-table border>
+        <el-table-column align="center" label="名称"></el-table-column>
+        <el-table-column align="center" label="标识"></el-table-column>
+        <el-table-column align="center" label="描述"></el-table-column>
+        <el-table-column align="center" label="操作">
+          <template>
+            <el-button type="text">添加</el-button>
+            <el-button type="text">编辑</el-button>
+            <el-button type="text">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </div>
   </div>
 </template>
