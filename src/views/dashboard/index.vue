4c1dc2f331262bc8746319b9eaf15523396b780b
zhao-feifei <398983914@qq.com>
2021-09-22 20:40:35 +0800

    雷达图完成

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -100,6 +100,7 @@
             <span>绩效指数</span>
           </div>
           <!-- 放置雷达图 -->
+          <radar></radar>
         </el-card>
         <!-- 帮助连接 -->
         <el-card class="box-card">
@@ -139,11 +140,13 @@ import { createNamespacedHelpers } from 'vuex'
 const { mapState } = createNamespacedHelpers('user')
 import { mapGetters } from 'vuex'
 import WorkCalendar from './components/work-calendar'
+import Radar from './components/radar'
 
 export default {
   name: 'Dashboard',
   components: {
-    WorkCalendar
+    WorkCalendar,
+    Radar
   },
   data() {
     return {
